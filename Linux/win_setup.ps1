92fc8e02197eddd33e263af4bfe40d0c52eb1991
cryoelite <arvindsagar702@gmail.com>
2024-10-25 01:05:40 +0530

    Win_setup finally complete

--- Linux/win_setup.ps1
+++ Linux/win_setup.ps1
@@ -1,173 +1,266 @@
 
-Write-Output "Starting Windows Setup Script win_setup.ps1"
-Write-Output "Requires seed.img, which is generated on any linux host with cloud-localds seed.img user-data.yaml metadata.yaml"
+Write-Host "Starting Windows Setup Script win_setup.ps1"
+Write-Host "Requires seed.img, which is generated on any linux host with cloud-localds seed.img user-data.yaml metadata.yaml"
 
 function Test-IsAdmin {
     $currentUser = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
     return $currentUser.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 }
 
-if (-not (Test-IsAdmin)) {
-    Write-Output "Please run as Admin"
-    exit 1
-}
-try {
+function check_hyper_v {
+
+    Write-Host "Checking for Hyper-V"
     $hypervFeature = dism.exe /Online /Get-FeatureInfo /FeatureName:Microsoft-Hyper-V-All
     if (-not ($hypervFeature -like "*State : Enabled*")) {
-        Write-Output "Enabling Hyper V"
+        Write-Host "Enabling Hyper V"
         Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
     }
-    Write-Output "Hyper V enabled"
-}
-catch {
-    Write-Error "Error $_"
-    exit 1
+    Write-Host "Hyper V is enabled"
 }
 
-winget.exe install QEMU
+function download_image {
+    param (
+        [string]$img_path,
+        [string]$arch,
+        [string]$arm64_vm_url,
+        [string]$amd64_vm_url
+    )
 
-$vm_name = "crubuntu"
-$memory_size = "4G"
-$disk_size = "20G"   
-$cpu_cores = 2
-$seed_img = ".\seed.img"
-
-try {
-    if (-not (Test-Path $seed_img)) {
-        Write-Error "$seed_img doesn't exist, can't continue"
-        exit 1
-    }
-    else {
-        Write-Output "$seed_img exists, continuing"
-    }
-}
-catch {
-    Write-Error "Error $_"
-    exit 1
-}
-Write-Output "Checking architecture"
-$arch = "unknown"
-$system_arch = [System.Runtime.InteropServices.RuntimeInformation]::ProcessArchitecture
-
-if ($system_arch -eq "Arm64") {
-    Write-Output "Platform: arm64"
-    $arch = "arm64"
-}
-elseif ($system_arch -eq "X64") {
-    Write-Output "Platform: amd64"
-    $arch = "amd64"
-}
-else {
-    Write-Output "Unknown platform: $system_arch"
-    exit 1
-}
-
-# https://www.reddit.com/r/Ubuntu/comments/8zyt3p/comment/e2mldhj/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button 
-# and
-# https://www.reddit.com/r/Ubuntu/comments/29boes/comment/cije5ir/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
-
-$imgPath = "$env:TMP\ubuntu-oracular-server-cloudimg-$arch.img"
-Write-Output "Checking\Downloading and attaching iso $imgPath"
-
-if (Test-Path $imgPath) {
-    Write-Output "$imgPath already exists"
-}
-else {
-    Write-Output "$imgPath doesn't exist, downloading"
+    Write-Host "Downloading $img_path"
     $url = ""
+    # https://www.reddit.com/r/Ubuntu/comments/8zyt3p/comment/e2mldhj/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button 
+    # and
+    # https://www.reddit.com/r/Ubuntu/comments/29boes/comment/cije5ir/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+
     if ($arch -eq "amd64") {
-        $url = "https://cloud-images.ubuntu.com/oracular/current/oracular-server-cloudimg-amd64.img"
+        $url = $amd64_vm_url
     }
     else {
-        $url = "https://cloud-images.ubuntu.com/oracular/current/oracular-server-cloudimg-arm64.img"
+        $url = $arm64_vm_url
     }
 
-    try {
-        Write-Output "Downloading $url"
-        Invoke-WebRequest -Uri $url -OutFile $imgPath
-        Write-Output "Download completed successfully. Saved as '$imgPath'."
+
+    Write-Host "Downloading $url"
+    Invoke-WebRequest -Uri $url -OutFile $img_path
+    Write-Host "Download completed successfully. Saved as '$img_path'."
+
+
+}
+
+function setup_image {
+    param (
+        [string]$arch,
+        [string]$img_path,
+        [string]$url_sha256,
+        [string]$arm64_vm_url,
+        [string]$amd64_vm_url
+    )
+
+    Write-Host "Checking\Downloading and attaching image $img_path"
+
+    if (Test-Path $img_path) {
+        Write-Host "$img_path exists, verifying SHA256 Checksum"
+        Write-Host "Downloading SHA256 Checksum"
+        
+        #$url_sha256_gpg = "https://cloud-images.ubuntu.com/oracular/current/SHA256SUMS.gpg" Not verifying GPG right now
+        $sha256_file = "$env:TMP\SHA256SUMS"
+        Invoke-WebRequest -Uri $url_sha256 -OutFile $sha256_file
+
+        #Write-Host "Installing GPG4Win to get gpg to verify checksums"
+        #winget.exe install -e --id GnuPG.Gpg4win
+        #gpg.exe --verify SHA256SUMS.gpg SHA256SUMS
+        $calculatedChecksum = (CertUtil -hashfile $img_path SHA256)[1].Trim()
+    
+        Write-Host "Calculated checksum: $calculatedChecksum for $img_path"
+        Write-Host "Looking for checksum in $sha256_file..."
+        $content = Get-Content -Path $sha256_file -Raw
+        $lines = $content -split "`n"
+
+        $expectedChecksum = $null
+        Write-Host "Iterating over lines of the checksum file to locate the image ending with $img_path's ending"
+        foreach ($line in $lines) {
+            if ($line.Length -gt 67) {
+                $imagename_in_file = $line.Substring(66)
+                if ($img_path.EndsWith($imagename_in_file)) {
+                    $expectedChecksum = $line.Substring(0, 64);
+                    Write-Host "Found checksum $expectedChecksum for $imagename_in_file in $sha256_file"
+                }
+            }
+        }
+        
+        if ($null -eq $expectedChecksum) {
+            Write-Error "Error: Checksum not found for $img_path in $sha256_file. Cannot continue"
+            throw "Checksum mismatch for $img_path, not found in $sha256_file"
+        }
+
+        Write-Host "Our checksum $calculatedChecksum and Expected checksum: $expectedChecksum"
+        if ($calculatedChecksum -eq $expectedChecksum) {
+            Write-Host "Checksum verified successfully! The file is valid."
+        }
+        else {
+            Write-Host "Checksum verification failed! The file isn't up-to-date or corrupted."
+            download_image -imgPath $img_path -arch $arch -arm64_vm_url $arm64_vm_url -amd64_vm_url $amd64_vm_url
+        }  
+
+
     }
+    else {
+        download_image -img_path $img_path -arch $arch -arm64_vm_url $arm64_vm_url -amd64_vm_url $amd64_vm_url
+    }
+}
+
+    
+function start_qemu {
+    param ([bool]$should_cloud_init, 
+        [string]$vm_name,
+        [string]$memory_size,
+        [string]$cpu_cores,
+        [string]$disk_path,
+        [string]$seed_img,
+        [string]$qemu_path,
+        [string]$state_sav_path
+    )
+    Write-Host "Setting up and Starting QEMU"
+
+
+    # https://powersj.io/posts/ubuntu-qemu-cli/
+    $qemu_args = @(
+        "-name", $vm_name
+        "-machine", "accel=whpx,type=q35"
+        "-m", $memory_size 
+        "-smp", "cores=$cpu_cores"
+        "-nographic"
+        "-device", "virtio-net-pci,netdev=net0"
+        "-netdev", "user,id=net0,hostfwd=tcp::2222-:22"
+        "-drive", "if=virtio,format=qcow2,file=$disk_path"
+
+    )
+    if ($should_cloud_init) {
+        $qemu_args += "-drive", "if=virtio,format=raw,file=$seed_img"
+    }
+
+    $qemu_command = $qemu_path + " " + ($qemu_args -join " ")
+
+    Write-Host "Starting QEMU $vm_name"
+    Start-Process alacritty `
+        -ArgumentList @("-e", $qemu_command) `
+
+    Write-Host "QEMU $vm_name started. \n Note: Shutdown with 'shutdown -h now' inside the VM to save state, killing the process does not save state"
+}
+
+function get_arch {
+    
+    Write-Host "Getting architecture"
+    $arch = "unknown"
+    $system_arch = [System.Runtime.InteropServices.RuntimeInformation]::ProcessArchitecture
+
+    if ($system_arch -eq "Arm64") {
+        Write-Host "Platform: arm64"
+        $arch = "arm64"
+    }
+    elseif ($system_arch -eq "X64") {
+        Write-Host "Platform: amd64"
+        $arch = "amd64"
+    }
+    else {
+        Write-Host "Unknown platform: $system_arch"
+        throw "Unknown platform: $system_arch"
+    }
+    Write-Host "Architecture: $arch"
+    return $arch    
+}
+
+function setup_disk {
+    param (
+        [string]$img_path,
+        [string]$disk_path,
+        [string]$disk_folder,
+        [string]$disk_size,
+        [string]$qemu_img_path
+    )
+    Write-Host "Setting up disk at $disk_path"
+
+    Write-Host "Making Directory $disk_folder"
+    New-Item -ItemType Directory -Path $disk_folder -Force
+
+    <#     Write-Host "Generating qcow2 disk from $img_path"
+    
+    Start-Process -FilePath $qemu_img_path `
+        -ArgumentList @("convert", "-f", "raw", "-O", "qcow2", $img_path, $disk_path) `
+        -NoNewWindow -Wait #>
+    Write-Host "Copying $img_path to $disk_path"
+    Copy-Item -Path $img_path -Destination $disk_path
+
+    Write-Host "Successfully finished generating $disk_path"
+ 
+    Write-Host "Resizing $disk_path"
+    Start-Process -FilePath $qemu_img_path `
+        -ArgumentList @("resize", $disk_path, $disk_size) `
+        -NoNewWindow -Wait
+    Write-Host "Successfully resized $disk_path to $disk_size" 
+}
+function main {
+    if (-not (Test-IsAdmin)) {
+        Write-Host "Please run as Admin"
+        throw "Not running as admin"
+    }
+    try {
+        Write-Host "Installing QEMU"
+        winget.exe install QEMU
+
+        $vm_name = "crubuntu"
+        $memory_size = "4G"
+        $disk_size = "20G"
+        $disk_folder = "C:\NFBase\VMs\"
+        $disk_path = "C:\NFBase\VMs\$vm_name.img"   
+        $disk_save_file = "C:\NFBase\VMs\${vm_name}_state.sav"   
+        $cpu_cores = 2
+        $seed_img = ".\seed.img"
+        $qemu_img_path = "C:\Program Files\qemu\qemu-img.exe"
+        $qemu_path = "C:\Program Files\qemu\qemu-system-x86_64.exe"
+        $url_sha256 = "https://cloud-images.ubuntu.com/oracular/current/SHA256SUMS"
+        $arm64_vm_url = "https://cloud-images.ubuntu.com/oracular/current/oracular-server-cloudimg-arm64.img"
+        $amd64_vm_url = "https://cloud-images.ubuntu.com/oracular/current/oracular-server-cloudimg-amd64.img"
+        $arch = get_arch
+        $img_path = "$env:TMP\ubuntu-oracular-server-cloudimg-$arch.img"
+        $cleanup = $false
+        
+        if ($cleanup) {
+            Write-Host "Cleaning up VM and its files (except seed.img)"
+            Remove-Item $disk_path -Force
+            Remove-Item $img_path -Force
+            Write-Host "Cleanup complete"
+        }
+        else {
+            Write-Host "Checking if $disk_path exists" 
+            if ((Test-Path $disk_path)) {
+                Write-Host "$disk_path already exists, starting the VM directly"
+                start_qemu -should_cloud_init $false -vm_name $vm_name -memory_size $memory_size -cpu_cores $cpu_cores -disk_path $disk_path -seed_img $seed_img -qemu_path $qemu_path -state_sav_path $disk_save_file
+            }
+            else {
+                Write-Host "$disk_path doesn't exist, creating it (and the VM)"
+                if (-not (Test-Path $seed_img)) {
+                    Write-Error "$seed_img doesn't exist, can't continue"
+                    throw "No seed image $seed_img"
+                }
+
+                setup_image -arch $arch -img_path $img_path -arm64_vm_url $arm64_vm_url -amd64_vm_url $amd64_vm_url -url_sha256 $url_sha256
+
+                setup_disk -img_path $img_path -disk_path $disk_path -disk_folder $disk_folder -disk_size $disk_size -qemu_img_path $qemu_img_path
+
+                start_qemu -should_cloud_init $true -vm_name $vm_name -memory_size $memory_size -cpu_cores $cpu_cores -disk_path $disk_path -seed_img $seed_img -qemu_path $qemu_path -state_sav_path $disk_save_file
+            }
+            # ssh -p 2222 localhost
+            # ssh ubuntu@localhost -p 2222
+            exit 0
+        }
+    }
+
     catch {
-        Write-Output "Error downloading the file: $_"
-        exit 1 
+        Write-Error "Error $_"
+        exit 1
     }
     
 }
-Write-Output "Installing Python 3.13 or skipping if it is installed"
-try {
-    $python3_path = "$env:TMP\python-3.13.0-$arch.exe"
-    if (Test-Path $python3_path) {
-        Write-Output "Python 3.13 already downloaded"
-    }
-    else {
-        Write-Output "Downloading Python 3.13"
-        Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.13.0/python-3.13.0-$arch.exe" -OutFile "$env:TMP\python-3.13.0-$arch.exe"
-        Write-Output "Download completed successfully. Saved as '$python3_path'."
-    }
-    Write-Output "Installing Python 3.13"
-    Start-Process -FilePath $python3_path -Wait 
-    Write-Output "Installation finished for Python 3.13."
-}
-catch {
-    Write-Output "Error with python installation: $_"
-    exit 1 
-}
 
-
-
-<# Write-Output "Setting up virtual switch for the VM, if it doesn't already exist"
-$switchName = "crswitch" 
-try {
-     
-    $adapterName = "Ethernet" 
-    $virtualSwitch = Get-VMSwitch -Name $switchName -ErrorAction SilentlyContinue
-
-    if (-not $virtualSwitch) {
-        Write-Output "Virtual Switch $switchName doesn't exist, creating it"
-        New-VMSwitch -Name $switchName -NetAdapterName $adapterName 
-        Write-Host "Created virtual switch: $switchName"
-    }
-    else {
-        Write-Host "Virtual switch already exists: $switchName"
-    }
-    Write-Host "Virtual switch $switchName setup complete"
-}
-catch {
-    Write-Output "Failed to set up/check switch: $_"
-    exit 1 
-} #>
-#If python was installed, it won't be in this script's environment, but the other shell will have it anyway, so good thing our python server runs in another window.
-Write-Output "Starting Python server to serve cloud-init files to the VM"
-$process = Start-Process alacritty `
-    -ArgumentList '-e', 'python.exe -m http.server --directory .' `
-    -PassThru
-Write-Output "Python process Started"
-
-Write-Host "Setting up and Starting QEMU"
-$qemu_path = "C:\Program Files\qemu\qemu-system-x86_64.exe"
-# https://powersj.io/posts/ubuntu-qemu-cli/
-$qemu_args = @(
-    "-name", $vm_name
-    "-machine", "accel=whpx,type=q35"
-    "-m", $memory_size 
-    "-smp", "cores=$cpu_cores"
-    "-nographic"
-    "-device", "virtio-net-pci,netdev=net0"
-    "-netdev", "user,id=net0,hostfwd=tcp::2222-:22"
-    "-drive", "if=virtio,format=qcow2,file=$imgPath"
-    "-drive", "if=virtio,format=raw,file=$seed_img"
-)
-
-Start-Process -FilePath $qemu_path -ArgumentList $qemu_args -PassThru -Wait
-
-Write-Host "QEMU $vm_name started"
-Write-Output "Stopping Python server"
-Stop-Process -Id $process.Id
-Write-Output "Stopped Python server"
-
-# ssh -p 2222 localhost
-# Optional: After VM setup, you can connect via SSH (ensure port forwarding)
-# ssh ubuntu@localhost -p 2222
-
-
-exit 0
+main
