7f750c21a656270509489e341bef933e5d781f57
cryoelite <arvindsagar702@gmail.com>
2024-10-24 07:36:11 +0530

    Intermediary commit

--- Linux/win_setup.ps1
+++ Linux/win_setup.ps1
@@ -1,5 +1,6 @@
 
 Write-Output "Starting Windows Setup Script win_setup.ps1"
+Write-Output "Requires seed.img, which is generated on any linux host with cloud-localds seed.img user-data.yaml metadata.yaml"
 
 function Test-IsAdmin {
     $currentUser = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
@@ -10,57 +11,12 @@ if (-not (Test-IsAdmin)) {
     Write-Output "Please run as Admin"
     exit 1
 }
-
-winget.exe install QEMU
-
-$vmName = "cr_Ubuntu"
-$memoryStartupBytes = 4GB
-$vmPath = "C:\VMs\$vmName"
-$vhdPath = "$vmPath\$vmName.vhd"
-#$switchName = "cr_ExternalSwitch"
-$switchName = "Default Switch"
-Write-Output "Using default values"
-
 try {
-
-    Write-Output "Checking if VM $vmName exists"
-    $vm = Get-VM -Name $vmName -ErrorAction SilentlyContinue
-
-    if ($vm) {
-        Write-Output "VM '$vmName' exists. Removing it"
-
-        # Stop the VM if it's running
-        if ($vm.State -eq 'Running') {
-            Stop-VM -Name $vmName -Force
-            Write-Output "VM '$vmName' stopped."
-        }
-
-        Remove-VM -Name $vmName -Force
-        Write-Output "VM '$vmName' removed."
-    
+    $hypervFeature = dism.exe /Online /Get-FeatureInfo /FeatureName:Microsoft-Hyper-V-All
+    if (-not ($hypervFeature -like "*State : Enabled*")) {
+        Write-Output "Enabling Hyper V"
+        Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
     }
-    Write-Output "Checking if VHD $vhdPath exists"
-    if (Test-Path $vhdPath) {
-        Write-Output "VHD file '$vhdPath' exists. Removing VHD..."
-        Remove-Item -Path $vhdPath -Force
-        Write-Output "VHD file '$vhdPath' removed."
-    }
-    <#   
-    Write-Output "Checking if VM Switch $switchName exists"
-   $switch = Get-VMSwitch -Name $switchName -ErrorAction SilentlyContinue
-    if ($switch) {
-        Write-Output "Switch $switchName exists, removing"
-        Remove-VMSwitch -Name $switchName -Force
-        Write-Output "Switch $switchName removed"
-    } #>
-}
-catch {
-    Write-Error "Error $_"
-    exit 1
-}
-try {
-    Write-Output "Enabling Hyper V"
-    Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
     Write-Output "Hyper V enabled"
 }
 catch {
@@ -68,128 +24,150 @@ catch {
     exit 1
 }
 
+winget.exe install QEMU
+
+$vm_name = "crubuntu"
+$memory_size = "4G"
+$disk_size = "20G"   
+$cpu_cores = 2
+$seed_img = ".\seed.img"
+
 try {
-    Write-Output "Checking architecture"
-    $arch = [System.Runtime.InteropServices.RuntimeInformation]::ProcessArchitecture
-    if ($arch -eq "Arm64") {
-        Write-Output "Platform: arm64"
-        $arch = "arm64"
-    }
-    elseif ($arch -eq "X64") {
-        Write-Output "Platform: amd64"
-        $arch = "amd64"
-    }
-    else {
-        Write-Output "Unknown platform: $arch"
+    if (-not (Test-Path $seed_img)) {
+        Write-Error "$seed_img doesn't exist, can't continue"
         exit 1
     }
+    else {
+        Write-Output "$seed_img exists, continuing"
+    }
+}
+catch {
+    Write-Error "Error $_"
+    exit 1
+}
+Write-Output "Checking architecture"
+$arch = "unknown"
+$system_arch = [System.Runtime.InteropServices.RuntimeInformation]::ProcessArchitecture
 
-    # https://www.reddit.com/r/Ubuntu/comments/8zyt3p/comment/e2mldhj/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button 
-    # and
-    # https://www.reddit.com/r/Ubuntu/comments/29boes/comment/cije5ir/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+if ($system_arch -eq "Arm64") {
+    Write-Output "Platform: arm64"
+    $arch = "arm64"
+}
+elseif ($system_arch -eq "X64") {
+    Write-Output "Platform: amd64"
+    $arch = "amd64"
+}
+else {
+    Write-Output "Unknown platform: $system_arch"
+    exit 1
+}
 
-    $imgPath = "$env:TMP\ubuntu-oracular-server-cloudimg-$arch.img"
-    Write-Output "Checking\Downloading and attaching iso $imgPath"
+# https://www.reddit.com/r/Ubuntu/comments/8zyt3p/comment/e2mldhj/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button 
+# and
+# https://www.reddit.com/r/Ubuntu/comments/29boes/comment/cije5ir/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
 
-    if (Test-Path $imgPath) {
-        Write-Output "$imgPath already exists"
+$imgPath = "$env:TMP\ubuntu-oracular-server-cloudimg-$arch.img"
+Write-Output "Checking\Downloading and attaching iso $imgPath"
+
+if (Test-Path $imgPath) {
+    Write-Output "$imgPath already exists"
+}
+else {
+    Write-Output "$imgPath doesn't exist, downloading"
+    $url = ""
+    if ($arch -eq "amd64") {
+        $url = "https://cloud-images.ubuntu.com/oracular/current/oracular-server-cloudimg-amd64.img"
     }
     else {
-        Write-Output "$imgPath doesn't exist, downloading"
-        $url = ""
-        if ($arch -eq "amd64") {
-            $url = "https://cloud-images.ubuntu.com/oracular/current/oracular-server-cloudimg-amd64.img"
-        }
-        else {
-            $url = "https://cloud-images.ubuntu.com/oracular/current/oracular-server-cloudimg-arm64.img"
-        }
+        $url = "https://cloud-images.ubuntu.com/oracular/current/oracular-server-cloudimg-arm64.img"
+    }
 
-        try {
-            Write-Output "Downloading $url"
-            Invoke-WebRequest -Uri $url -OutFile $imgPath
-            Write-Output "Download completed successfully. Saved as '$imgPath'."
-        }
-        catch {
-            Write-Output "Error downloading the file: $_"
-            exit 1  # Exit the script on download failure
-        }
+    try {
+        Write-Output "Downloading $url"
+        Invoke-WebRequest -Uri $url -OutFile $imgPath
+        Write-Output "Download completed successfully. Saved as '$imgPath'."
+    }
+    catch {
+        Write-Output "Error downloading the file: $_"
+        exit 1 
+    }
     
-    }
-
-    $rawPath = "$env:TMP\ubuntu-24.10-server-cloudimg-$arch.raw"
-    if (Test-Path $rawPath) {
-        Remove-Item -Path $rawPath -Force
-    }
-    #Write-Output "Converting and storing $imgPath to $rawPath and then to $vhdPath"
-    Write-Output "Converting and storing $imgPath to $vhdPath"
-    #https://superuser.com/a/1093615/1088896
-    $qemu_img_path = "C:\Program Files\qemu\qemu-img.exe"
-    #& $qemu_img_path convert -f qcow2 -O raw $imgPath $rawPath #Convert to raw, to ensure compression or encryption and ensure it is not sparse
-    #& $qemu_img_path convert -f raw  -O vpc -o subformat=dynamic $rawPath $vhdPath
-    fsutil.exe sparse setflag "$imgPath" 0
-    & $qemu_img_path convert -f qcow2  -O vpc -o subformat=dynamic $imgPath $vhdPath
-    fsutil.exe sparse setflag "$vhdPath" 0
-    Write-Output "Conversion and storage done for $vmName"
 }
-catch {
-    Write-Error "Error $_"
-    exit 1
-}
+Write-Output "Installing Python 3.13 or skipping if it is installed"
 try {
-<#     Write-Output "Creating new switch $switchName"
-    $SWITCH = @{
-        Name              = $switchName
-        NetAdapterName    = (Get-NetAdapter | Where-Object { $_.Status -eq "Up" } | Select-Object -First 1).Name 
-        AllowManagementOS = $true
+    $python3_path = "$env:TMP\python-3.13.0-$arch.exe"
+    if (Test-Path $python3_path) {
+        Write-Output "Python 3.13 already downloaded"
     }
-
-    New-VMSwitch @SWITCH  #>
-
-    $VM = @{
-        Name               = $vmName
-        MemoryStartupBytes = $memoryStartupBytes
-        Generation         = 2
-        Path               = $vmPath
-        SwitchName         = $switchName
-    
+    else {
+        Write-Output "Downloading Python 3.13"
+        Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.13.0/python-3.13.0-$arch.exe" -OutFile "$env:TMP\python-3.13.0-$arch.exe"
+        Write-Output "Download completed successfully. Saved as '$python3_path'."
     }
-    Write-Output "Creating New VM $vmName"
-    New-VM @VM 
-    Write-Output "New VM Created"
-
+    Write-Output "Installing Python 3.13"
+    Start-Process -FilePath $python3_path -Wait 
+    Write-Output "Installation finished for Python 3.13."
 }
 catch {
-    Write-Error "Error $_"
-    exit 1
+    Write-Output "Error with python installation: $_"
+    exit 1 
 }
 
+
+
+<# Write-Output "Setting up virtual switch for the VM, if it doesn't already exist"
+$switchName = "crswitch" 
 try {
-    Write-Output "Attaching $vhdPath to $vmName"
-    Add-VMHardDiskDrive -VMName $vmName -Path $vhdPath
-    Write-Output "Attached $vhdPath to $vmName"
+     
+    $adapterName = "Ethernet" 
+    $virtualSwitch = Get-VMSwitch -Name $switchName -ErrorAction SilentlyContinue
 
-    #Enabling Secure Boot
-    Set-VMFirmware -VMName $vmName -EnableSecureBoot On -SecureBootTemplate "MicrosoftUEFICertificateAuthority"
-
-    Write-Output "Starting $vmName"
-    Start-VM -Name $vmName
-    Write-Output "$vmName started"
-
-    Write-Output "Waiting 60 seconds for the VM to finish starting"
-    Start-Sleep -Seconds 60 
-    Write-Output "Done"
-
-    Get-VM -Name $vmName | Format-List
-    Get-VMNetworkAdapter -VMName $vmName
-
-    $vmIp = Get-VMNetworkAdapter -VMName $vmName | Select-Object -ExpandProperty IPAddresses
-
-    Write-Output "VM '$vmName' created and started."
-    Write-Output "IP Address: $vmIp"
-    Write-Output "You can now SSH into the VM using 'ssh ubuntu@$vmIp'."
+    if (-not $virtualSwitch) {
+        Write-Output "Virtual Switch $switchName doesn't exist, creating it"
+        New-VMSwitch -Name $switchName -NetAdapterName $adapterName 
+        Write-Host "Created virtual switch: $switchName"
+    }
+    else {
+        Write-Host "Virtual switch already exists: $switchName"
+    }
+    Write-Host "Virtual switch $switchName setup complete"
 }
 catch {
-    Write-Error "Error $_"
-    exit 1
-}
+    Write-Output "Failed to set up/check switch: $_"
+    exit 1 
+} #>
+#If python was installed, it won't be in this script's environment, but the other shell will have it anyway, so good thing our python server runs in another window.
+Write-Output "Starting Python server to serve cloud-init files to the VM"
+$process = Start-Process alacritty `
+    -ArgumentList '-e', 'python.exe -m http.server --directory .' `
+    -PassThru
+Write-Output "Python process Started"
+
+Write-Host "Setting up and Starting QEMU"
+$qemu_path = "C:\Program Files\qemu\qemu-system-x86_64.exe"
+# https://powersj.io/posts/ubuntu-qemu-cli/
+$qemu_args = @(
+    "-name", $vm_name
+    "-machine", "accel=whpx,type=q35"
+    "-m", $memory_size 
+    "-smp", "cores=$cpu_cores"
+    "-nographic"
+    "-device", "virtio-net-pci,netdev=net0"
+    "-netdev", "user,id=net0,hostfwd=tcp::2222-:22"
+    "-drive", "if=virtio,format=qcow2,file=$imgPath"
+    "-drive", "if=virtio,format=raw,file=$seed_img"
+)
+
+Start-Process -FilePath $qemu_path -ArgumentList $qemu_args -PassThru -Wait
+
+Write-Host "QEMU $vm_name started"
+Write-Output "Stopping Python server"
+Stop-Process -Id $process.Id
+Write-Output "Stopped Python server"
+
+# ssh -p 2222 localhost
+# Optional: After VM setup, you can connect via SSH (ensure port forwarding)
+# ssh ubuntu@localhost -p 2222
+
+
 exit 0
