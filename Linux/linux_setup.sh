b6fefe37caa9f9ace173d4bbdde08c92deb2168f
cryoelite <itscryonim@gmail.com>
2024-10-26 06:34:23 +0530

    linux_setup update

--- Linux/linux_setup.sh
+++ Linux/linux_setup.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-BASE_CONFIG_DIR=./Base_Config
+#Run with `sudo sh linux_setup.sh`
 
 apt-get update -y
 apt-get upgrade -y
 apt-get full-upgrade -y
 apt-get clean
 
-apt-get install -y docker.io nano
-cd $BASE_CONFIG_DIR
+apt-get install -y docker.io nano dirmngr gnupg software-properties-common curl gcc build-essential p7zip-full nano vim usbutils git \
+    python3 python3-venv \
+    clang gdb llvm
 
-touch .env
-echo "#Put environment variables here, check the $BASE_CONFIIG_DIR/docker-compose.yml for the variables needed" > .env
-nano .env
-docker-compose up -d
+curl --proto '=https' --tlsv1.3 https://sh.rustup.rs -sSf | sh -s -- -y                         #Rust
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" #Brew
+brew install node@23                                                                            #Node
+curl -fsSL https://deno.land/install.sh | sh                                                    #deno
 
 apt-get update -y
 apt-get upgrade -y
 apt-get full-upgrade -y
 apt-get clean
-
-echo "Finished. Do docker container attach <whatever container name you gave in .env> to attach"
